fc4e2cafe72a39c36a0cc9b9f604c3e4f4ec1372
曹实杭 <572078151@qq.com>
2018-02-25 12:42:49 +0800

    修改下五子棋的样式

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,6 +1,6 @@
 <template>
   	<span class="back-line" @click="clickGrid">
-  		<span v-if="data" class="grid" :style="background"></span>
+  		<span v-if="data" :class="['grid', color]"></span>
   	</span>
 </template>
 
@@ -10,10 +10,8 @@ export default {
 	name: 'grid',
 	props: ['data', 'site'],
 	computed: {
-		background() {
-			return {
-				backgroundColor: this.data === 1 ? 'black' : 'white'
-			}
+		color() {
+			return this.data === 1 ? 'black' : 'white'
 		}
 	},
 	methods: {
@@ -59,9 +57,28 @@ export default {
 	position: absolute;
 	width: 31px;
 	height: 31px;
-	border-radius: 15.5px;
+	border-radius: 50%;
 	left: 5px;
 	top: 5px;
 	z-index: 100;
+	box-shadow:2px 2px 4px rgba(0, 0, 0, 0.3);
+}
+.black{
+	background: radial-gradient(circle, #444, #222);
+}
+.grid:after{
+	content: "";
+    position: absolute;
+    width: 13px;
+    height: 7px;
+    border-radius: 50%;
+    top: 6px;
+    left: 2px;
+    transform: rotate(-45deg);
+    background: radial-gradient(circle at 6px 1px, #fff, rgba(255, 255, 255, 0.9));
+    box-shadow: 0 0 8px rgba(255, 255, 255, 0.7);
+}
+.white{
+	background: #f1f1f1;
 }
 </style>
